a4517ee6914fb8e483cf7112b9d0472c52f3c2dc
Will <will@containerlabs.io>
2022-06-18 16:32:50 -0500

    add iam securty block to bundle

--- src/_artifacts.tf
+++ src/_artifacts.tf
@@ -4,6 +4,20 @@ locals {
     url = local.full_url
   }
   data_security = {
+    iam = {
+      read = {
+        role      = "roles/storage.objectViewer"
+        condition = "resource.name.startsWith(\"projects/_/buckets/${google_storage_bucket.main.name}\")"
+      }
+      "read/write" = {
+        role      = "roles/storage.objectCreator"
+        condition = "resource.name.startsWith(\"projects/_/buckets/${google_storage_bucket.main.name}\")"
+      }
+      admin = {
+        role      = "roles/storage.objectAdmin"
+        condition = "resource.name.startsWith(\"projects/_/buckets/${google_storage_bucket.main.name}\")"
+      }
+    }
   }
   specs_gcp = {
     project = local.gcp_project_id
